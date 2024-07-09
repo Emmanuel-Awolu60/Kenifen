22d1387e41510235fe0b0cfc9eb051db83d55253
E O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2024-07-09 21:11:07 +0100

    Installed tailwind css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,10 @@
-import React, { useState } from "react";
-import "./Header.css";
-import { BiMenuAltRight } from "react-icons/bi";
-import OutsideClickHandler from "react-outside-click-handler";
+import React from "react";
+
 const Header = () => {
-  const [menuOpened, setMenuOpened] = useState(false);
-
-  const getMenuStyles = (menuOpened) => {
-    if (document.documentElement.clientWidth <= 800) {
-      return { right: !menuOpened && "-100%" };
-    }
-  };
   return (
-    <section className="h-wrapper">
-      <div className="h-container flexCenter paddings innerWidth">
-        <img src="./logo.png" alt="logo" width={100} />
-
-        <OutsideClickHandler
-          onOutsideClick={() => {
-            setMenuOpened(false);
-          }}
-        >
-          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-            <a href="">Residencies</a>
-            <a href="">Our Value</a>
-            <a href="">Contact Us</a>
-            <a href="">Get Started</a>
-            <button className="button">
-              <a href="">Contact</a>
-            </button>
-          </div>
-        </OutsideClickHandler>
-        <div
-          className="menu-icon"
-          onClick={() => setMenuOpened((prev) => !prev)}
-        >
-          <BiMenuAltRight size={30} />
-        </div>
-      </div>
-    </section>
+    <div>
+      <h1 className="c">How Are you Doing</h1>
+    </div>
   );
 };
 
