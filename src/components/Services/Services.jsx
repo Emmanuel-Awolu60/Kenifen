8b0402393b441b8ca90bb0eed561b7f4e8575f02
Emmanuel Awolu .O <114992756+Emmanuel-Awolu60@users.noreply.github.com>
2024-10-21 02:25:24 -0700

    updated

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,76 +3,76 @@ import React from 'react'
 export const Services = () => {
     return (
         <div>
-            <div class="my-generic text-center" id="services">
-                <h2 class="mb-3 mt-5 aquatico-regular">Services</h2>
-                <p class="roboto-regular text-center lead">
+            <div className="my-generic text-center" id="services">
+                <h2 className="mb-3 mt-5 aquatico-regular">Services</h2>
+                <p className="roboto-regular text-center lead">
                     Elevate your style with our suit fashion company, offering
                     impeccable craftsmanship, personalized fittings, and timeless
                     designs for a wardrobe of sophistication and confidence.
                 </p>
             </div>
 
-            <div class="row justify-content-center align-items-center my-5 mx-auto">
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+            <div className="row justify-content-center align-items-center my-5 mx-auto">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
-                            src="/assets/2-3966b115.jpg"
-                            class="card-img"
+                            src="./img/kenifenS1.jpg"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent">
-                            <h5 class="card-title aquatico-regular">Custom Tailoring</h5>
-                            <p class="card-text my-5 roboto-regular text-justify">
+                        <div className="card-img-overlay bg-transparent">
+                            <h5 className="card-title aquatico-regular">Custom Tailoring</h5>
+                            <p className="card-text my-5 roboto-regular text-justify">
                                 Our suit fashion company specializes in crafting bespoke suits
                                 tailored to your unique measurements and preferences, ensuring
                                 a perfect fit and personalized style.
                             </p>
-                            <p class="card-text">
+                            <p className="card-text">
                                 <i class="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
-                            src="/assets/5-79352e92.jpg"
-                            class="card-img"
+                            src="./img/kenifenS2.jpg"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent">
-                            <h5 class="card-title aquatico-regular">
+                        <div className="card-img-overlay bg-transparent">
+                            <h5 className="card-title aquatico-regular">
                                 Ready-to-Wear Collections
                             </h5>
-                            <p class="card-text my-5 roboto-regular text-justify">
+                            <p className="card-text my-5 roboto-regular text-justify">
                                 Explore our curated range of ready-to-wear suits featuring a
                                 variety of classic and contemporary designs, ideal for those
                                 seeking stylish options without the need for customization.
                             </p>
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
-                            src="/assets/6-06affbeb.jpg"
-                            class="card-img"
+                            src="./img/kenifenS3.jpg"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent">
-                            <h5 class="card-title aquatico-regular">
+                        <div className="card-img-overlay bg-transparent">
+                            <h5 className="card-title aquatico-regular">
                                 Alterations and Repairs
                             </h5>
-                            <p class="card-text my-5 roboto-regular text-justify">
+                            <p className="card-text my-5 roboto-regular text-justify">
                                 We offer expert alteration and repair services to ensure your
                                 suits always look their best, whether it's adjusting the fit,
                                 fixing minor damages, or refreshing your wardrobe.
                             </p>
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
@@ -80,16 +80,16 @@ export const Services = () => {
             </div>
 
             {/* <!-- call to action --> */}
-            <div class="container px-4 py-5 my-generic" id="hanging-icons">
-                <h2 class="pb-2 border-bottom text-center aquatico-regular">
+            <div className="container px-4 py-5 my-generic" id="hanging-icons">
+                <h2 className="pb-2 border-bottom text-center aquatico-regular">
                     Are you ready to get started?
                 </h2>
-                <div class="row g-4 py-5 row-cols-1 row-cols-lg-2">
+                <div className="row g-4 py-5 row-cols-1 row-cols-lg-2">
                     <div
-                        class="col d-flex align-items-start bg-white rounded-5 shadow-3 pt-5 hvr-sink"
+                        className="col d-flex align-items-start bg-white rounded-5 shadow-3 pt-5 hvr-sink"
                     >
-                        <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-                            <i class="bi bi-bag-check-fill"></i>
+                        <div className="icon-square bg-light text-dark flex-shrink-0 me-3">
+                            <i className="bi bi-bag-check-fill"></i>
                         </div>
                         <div>
                             <h2 class="roboto-bold">Unlock Your Sartorial Elegance</h2>
@@ -100,28 +100,28 @@ export const Services = () => {
                             </p>
                             <a
                                 href="#"
-                                class="btn btn-outline-info hvr-sweep-to-top btn-lg my-3 roboto-medium"
+                                className="btn btn-outline-info hvr-sweep-to-top btn-lg my-3 roboto-medium"
                             >
                                 Get Started
                             </a>
                         </div>
                     </div>
                     <div
-                        class="col d-flex align-items-start bg-white rounded-5 shadow-3 pt-5 hvr-sink"
+                        className="col d-flex align-items-start bg-white rounded-5 shadow-3 pt-5 hvr-sink"
                     >
-                        <div class="icon-square bg-light text-dark flex-shrink-0 me-3">
-                            <i class="bi bi-house-heart"></i>
+                        <div className="icon-square bg-light text-dark flex-shrink-0 me-3">
+                            <i className="bi bi-house-heart"></i>
                         </div>
                         <div>
-                            <h2 class="roboto-bold">Tailored to Perfection</h2>
-                            <p class="roboto-regular text-justify">
+                            <h2 className="roboto-bold">Tailored to Perfection</h2>
+                            <p className="roboto-regular text-justify">
                                 Redefine your wardrobe with extraordinary craftsmanship and
                                 statement-making styles. Embrace the suit that suits you and
                                 start your transformation today.
                             </p>
                             <a
                                 href="#"
-                                class="btn btn-outline-info hvr-sweep-to-top btn-lg my-3 roboto-medium"
+                                className="btn btn-outline-info hvr-sweep-to-top btn-lg my-3 roboto-medium"
                             >
                                 Dive In
                             </a>
@@ -131,146 +131,146 @@ export const Services = () => {
             </div>
 
             {/* <!-- portfolio --> */}
-            <div class="my-generic text-center" id="portfolio">
-                <h2 class="mb-3 mt-5 aquatico-regular">Portfolio</h2>
-                <p class="roboto-regular text-center lead">
+            <div className="my-generic text-center" id="portfolio">
+                <h2 className="mb-3 mt-5 aquatico-regular">Portfolio</h2>
+                <p className="roboto-regular text-center lead">
                     Explore our portfolio of exquisite suits, showcasing precision
                     tailoring and impeccable design, embodying the sophistication and
                     versatility we offer.
                 </p>
             </div>
 
-            <div class="row justify-content-center align-items-center my-5 mx-auto">
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+            <div className="row justify-content-center align-items-center my-5 mx-auto">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/2-3966b115.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/5-79352e92.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/6-06affbeb.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/3-d3187101.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/11-a0b41cbc.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/12-dc243fbe.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/4-616bbbbd.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/9-d48b2ace.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-md-4 hvr-bounce-in my-3">
-                    <div class="card bg-dark text-white">
+                <div className="col-md-4 hvr-bounce-in my-3">
+                    <div className="card bg-dark text-white">
                         <img
                             src="/assets/13-26ef6062.jpg"
-                            class="card-img"
+                            className="card-img"
                             alt="Stony Beach"
                         />
-                        <div class="card-img-overlay bg-transparent2">
-                            <p class="card-text">
-                                <i class="bi bi-bag-check-fill"></i>
+                        <div className="card-img-overlay bg-transparent2">
+                            <p className="card-text">
+                                <i className="bi bi-bag-check-fill"></i>
                             </p>
                         </div>
                     </div>
